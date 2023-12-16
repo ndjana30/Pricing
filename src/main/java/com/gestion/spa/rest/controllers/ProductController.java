3508efb03108a2933e784f6d5fe22109053919e7
alphadigitalservices237 <alphadigitalservices237@gmail.com>
2023-12-16 13:57:54 +0100

    new commit

--- src/main/java/com/gestion/spa/rest/controllers/ProductController.java
+++ src/main/java/com/gestion/spa/rest/controllers/ProductController.java
@@ -24,7 +24,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.eclipse.jgit.*;
 
 @RestController
 @RequestMapping("api/v1/product")
